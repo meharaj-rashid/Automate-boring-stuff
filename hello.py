cf7ee8eceb2c52c53563b36eecc746e4cb69dfe2
meharaj <meharajh9@gmail.com>
2023-06-02 22:04:25 +0330

    Adds hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+#this program sya hello and asks user for his name
+
+
+print('Hello world')
+name=input('Whats is your name.? ')
+print('Hello '+ name +' .')
+age=input(name+', CAn you tell us ypur age..?')
+print(name+' next year you will be '+str(int(age)+1)+' years old.')
